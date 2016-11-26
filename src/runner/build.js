9e76d3d5ecc923addccbd24b02ef21398da8dde3
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-11-26 17:29:14 -0500

    Add logging to runner

--- src/runner/build.js
+++ src/runner/build.js
@@ -6,7 +6,11 @@ import neato from '../index'
 export default (neatoConfig: NeatoConfigType) => new Promise((resolve: Function, reject: Function) => {
   const compiler = webpack(neato.webpackConfig)
 
+  console.log('Going to run the build command.')
+
   compiler.run((err: any, stats: Object) => {
+    console.log('Running the build command.')
+
     if (err || stats.toJson().errors) {
       if (err) {
         console.error(err.stack || err)
